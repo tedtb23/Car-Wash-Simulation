b760ddf77717bd331b1c0a1f55f12c027112c74c
Ted <tedtbarnaby@gmail.com>
2024-01-11 04:14:45 -0600

    update readme and folder structure

--- README.md
+++ README.md
@@ -3,15 +3,17 @@ Simple simulation of a car wash with a flexible pricing structure that determine
 
 Format the input file as arrivalTime(int) price(float).
 For example, an input file could look like...
-0 4.00
-0 3.25
-0 2.10
-0 5.25
-2 0.50
-3 9.00
-4 20.00
-4 5.00
-5 100.00
-5 2.00
-5 35.00
-5 0.25
+0 4.00\
+0 3.25\
+0 2.10\
+0 5.25\
+2 0.50\
+3 9.00\
+4 20.00\
+4 5.00\
+5 100.00\
+5 2.00\
+5 35.00\
+5 0.25\
+
+This sample input is provided in sampleInput.txt in the project root directory.
---
+++ src/main/java/PriorityQueue.java
@@ -0,0 +1,103 @@
+/**
+ * Implements a priority queue.
+ * 
+ * @author Ted Barnaby
+ * @version 4/1/23
+ *
+ * @param <E> Any type that extends comparable of a type that is a super class of E or E itself.
+ */
+public class PriorityQueue<E extends Comparable<? super E>> {
+	private E[] data;
+	private int currSize;
+	private static final int DEFAULT_SIZE = 10;
+	
+	/**
+	 * Initialize an empty priority queue.
+	 */
+	PriorityQueue() {
+		data = (E[]) new Comparable[DEFAULT_SIZE];
+		currSize = 0;
+	}
+	
+	/**
+	 * Add a new element to the priority queue.
+	 * 
+	 * @param element The element to enqueue.
+	 */
+	public void enqueue(E element) {
+		currSize++;
+		if(currSize >= data.length) doubleQueue();
+		data[currSize - 1] = element;	
+	}
+	
+	/**
+	 * Get but do not remove the highest
+	 * priority element in the priority 
+	 * queue.
+	 * 
+	 * @return The highest priority element.
+	 */
+	public E peek() {
+		if(currSize == 0) return null;
+		if(currSize == 1) return data[currSize - 1];
+		
+		int p = 0;
+		for(int i = 1; i < currSize; i++) 
+			if(data[i].compareTo(data[p]) > 0) 
+				p = i;
+
+		return data[p];
+	}
+	
+	/**
+	 * Return and remove the highest
+	 * priority element in the priority 
+	 * queue.
+	 * 
+	 * @return The highest priority element.
+	 */
+	public E dequeue() {
+		if(currSize == 0) return null;
+		if(currSize == 1) return data[--currSize];
+		
+		int p = 0;
+		for(int i = 1; i < currSize; i++) 
+			if(data[i].compareTo(data[p]) > 0) 
+				p = i;
+			
+		E element = data[p];
+		data[p] = data[--currSize];
+		return element;
+	}
+	
+	/**
+	 * @return If the priority queue is logically empty.
+	 */
+	public boolean isEmpty() {
+		return currSize == 0;
+	}
+	
+	/**
+	 * Makes the priority queue logically empty.
+	 */
+	public void makeEmpty() {
+		currSize = 0;
+	}
+	
+	/**
+	 * @return The size of the priority queue.
+	 */
+	public int size() {
+		return currSize;
+	}
+	
+	/**
+	 * Doubles the current size of the array
+	 * that represents the priority queue.
+	 */
+	private void doubleQueue() {
+		E[] newData = (E[]) new Comparable[data.length * 2];
+		for(int i = 0; i < currSize; i++) newData[i] = data[i];
+		data = newData;
+	}
+}
